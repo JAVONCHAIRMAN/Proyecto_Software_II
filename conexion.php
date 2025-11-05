b0ea4e7133e8f5560d345fc69f7094b20541172e
Adrián Matute Corrales <155648966+JAVONCHAIRMAN@users.noreply.github.com>
2025-11-04 18:01:15 -0600

    conexion
    Traemos el archivo de conexion

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+//Agregue los datos correspondientes de su conexion a su base de datos
+    $conexion = mysqli_connect('bpeqhngzxpt1c0wno90c-mysql.services.clever-cloud.com','uzdq7daszfhif51j','ag52YpiVuos5U4AedZHS','bpeqhngzxpt1c0wno90c');
+  if (mysqli_connect_errno()) {
+    printf("Falló la conexión: %s\n", mysqli_connect_error());
+    exit();
+    }
+
+?>
